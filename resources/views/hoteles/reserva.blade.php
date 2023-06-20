b8ccf239c11fa0ae262d9ffe18e0f036b628e526
Luisj122 <luisjmnz122@gmail.com>
2023-06-20 13:11:56 +0200

    final

--- resources/views/hoteles/reserva.blade.php
+++ resources/views/hoteles/reserva.blade.php
@@ -50,44 +50,98 @@
             
             
             <div class="col-md-9">
-              @foreach ($hoteles as $value)
-              @php
-                  $habitaciones = App\Models\Habitaciones::where('hotel_id', $value->id)->get();
-                  $cont = $habitaciones->count();
-              @endphp
-          
-              @if ($cont != 0 && $value->usuario_id != Auth::user()->id)
-                  @php
+             
+                @foreach ($hoteles as $value)
+                    @php
+                        $habitaciones = App\Models\Habitaciones::where('hotel_id', $value->id)->get();
+                        $cont = $habitaciones->count();
+                    @endphp
+
+                  @if ($cont != 0 && $value->usuario_id != Auth::user()->id)
+
+                    @php
                       $libre = $habitaciones->where('disponibilidad', 'libre')->count();
-                  @endphp
-          
+                    @endphp
+      
+
                   <form action='/detalle/reserva/{{ $value->id}}' method="GET" enctype="multipart/form-data">
-                      @csrf
-                      <div class="card mb-4">
-                          <div class="row no-gutters">
-                              <div class="col-md-4">
-                                  <img src="{{ asset($value->imagen) }}" class="card-img img-fluid rounded" alt="Imagen" style="object-fit: cover; height: 100%;">
-                              </div>
-                              <div class="col-md-8">
-                                  <div class="card-body">
-                                      <h1 class="card-title" style="font-family: 'Noto Sans', sans-serif; font-family: 'Roboto', sans-serif;"><strong>{{ $value->nombre }}</strong></h3>
-                 
-          
-                                      @if ($cont <= 5 && $cont >= 1)
-                                          @if ($libre > 0)
+                        @csrf 
+                    <div class="card mb-4 ">
+                        <div class="row no-gutters">
+                            <div class="col-md-4">
+                                <img src="{{ asset($value->imagen) }}" class="card-img img-fluid rounded" alt="Imagen" style="object-fit: cover; height: 100%;">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                  
+                        
+                                    <h1 class="card-title" style="font-family: 'Noto Sans', sans-serif;
+                                    font-family: 'Roboto', sans-serif;"><strong>{{ $value->nombre }}</strong></h3>
+                                    @switch($value->categoria )
+                                        @case("uno")
+                                            <i class="fas fa-star text-warning"></i>
+                                            @break
+                                        @case("dos")
+                                            <i class="fas fa-star text-warning"></i>
+                                            <i class="fas fa-star text-warning"></i>
+                                            @break
+                                        @case("tres")
+                                            <i class="fas fa-star text-warning"></i>
+                                            <i class="fas fa-star text-warning"></i>
+                                            <i class="fas fa-star text-warning"></i>
+                                            @break
+                                        @case("cuatro")
+                                            <i class="fas fa-star text-warning"></i>
+                                            <i class="fas fa-star text-warning"></i>
+                                            <i class="fas fa-star text-warning"></i>
+                                            <i class="fas fa-star text-warning"></i>
+                                            @break
+                                        @case("cinco")
+                                            <i class="fas fa-star text-warning"></i>
+                                            <i class="fas fa-star text-warning"></i>
+                                            <i class="fas fa-star text-warning"></i>
+                                            <i class="fas fa-star text-warning"></i>
+                                            <i class="fas fa-star text-warning"></i>
+                                            @break
+                                        @default
+                                    @endswitch
+                      
+                                    <p class="card-text" style="font-family: 'Montserrat', sans-serif;
+                                    font-family: 'Noto Sans', sans-serif;
+                                    font-family: 'Roboto', sans-serif;">{{ $value->ciudad }}</p>
+                                    <p class="card-text" style="font-family: 'Montserrat', sans-serif;
+                                    font-family: 'Noto Sans', sans-serif;
+                                    font-family: 'Roboto', sans-serif;">{{ $value->pais }}</p>
+
+                                    <p class="card-text" style="font-family: 'Montserrat', sans-serif;
+                                    font-family: 'Noto Sans', sans-serif;
+                                    font-family: 'Roboto', sans-serif;">{{ $value->descripcion }}</p>
+
+                                      
+
+                                        @if ($cont <= 5 && $cont >= 1)
+                                          @if ($lire > 0)
                                               <p class="text-danger"><strong>¡Solo quedan {{ $libre }} habitaciones!</strong></p>
                                           @endif
-                                      @endif
+                                        @endif
+
+                
+                                    <input type="hidden" name="fechaEntrada" id="fechaEntrada" value="{{ $fechaEntrada }}">
+                                    <input type="hidden" name="fechaSalida" id="fechaSalida" value="{{ $fechaSalida }}">
+                                    <input type="hidden" name="ciudad" id="ciudad" value="{{ $ciudad }}">
           
-                                      <button class="btn btn-danger mt-4">Buscar</button>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </form>
-              @endif
-          @endforeach
-          
+                                    <button class="btn btn-danger mt-4">Buscar</button>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                      
+                  @endif
+                
+            @endforeach
+
       
                   
             </div>
